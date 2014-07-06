259b80b203dbd5063260737da672f80f657727f6
gsanta <santa_gergely@hotmail.com>
2014-07-06 23:05:06 +0200

    google.maps added

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,23 @@
 angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
+	$scope.loadMap = function() {
+		navigator.geolocation.getCurrentPosition( $scope.initialize );
+		//google.maps.event.addDomListener(window, 'load', $scope.initialize);
+	}
+
+	$scope.initialize = function( location ) {
+		console.log( location )
+
+		var mapProp = {
+			center:new google.maps.LatLng( location.coords.latitude, location.coords.longitude ),
+			zoom: 16,
+			mapTypeId:google.maps.MapTypeId.ROADMAP
+		};
+
+		var map = new google.maps.Map( document.getElementById( "googleMap" ), mapProp );
+	}
+
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
