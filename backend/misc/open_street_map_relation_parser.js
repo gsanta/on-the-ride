df003a57ee371ab206abaa5521980543e07c80c1
gsanta <santa_gergely@hotmail.com>
2014-07-13 14:30:21 +0200

    openstreatmap parser ready. Displaying fetched route on the map.

--- backend/misc/open_street_map_relation_parser.js
+++ backend/misc/open_street_map_relation_parser.js
@@ -1,6 +1,7 @@
 var xml2js = require('xml2js').parseString;
 var request = require('request');
 var fs = require('fs');
+var async = require("async")
 
 var isWriteToFileAsJson = false;
 
@@ -9,14 +10,40 @@ var relationUrl = 'http://api.openstreetmap.org/api/0.6/relation/2764576';
 loadResourceXmlAndParseToJs(relationUrl, false, function(result, isWriteToFileAsJson) {
 	var relation = extractRelationFromOsm(result, false);
 	var ways = extractWaysFromRelation(relation, false);	
-	console.log(ways[0].ref)
-	loadResourceXmlAndParseToJs("http://api.openstreetmap.org/api/0.6/way/" + ways[0].ref, false, function(result, isWriteToFileAsJson) {
-		var nds = extractNdsFromWay(result, false);
-		console.log(nds[0]);
-		loadResourceXmlAndParseToJs("http://api.openstreetmap.org/api/0.6/node/" + nds[0].ref, false, function(result, isWriteToFileAsJson) {
-			extractNodeFromOsm(result, true);
+
+	var nodes = []
+	var finalWays = [];
+
+
+	//console.log(ways[0])
+	for(var i = 0; i < ways.length; i++) {
+		loadResourceXmlAndParseToJs("http://api.openstreetmap.org/api/0.6/way/" + ways[i].ref, false, function(result, isWriteToFileAsJson) {
+			var nds = extractNdsFromWay(result, false);
+			finalWays.push(nds);
+			// finalWays[i] = {}
+			// finalWays[i].coords = [];
+
+			for(var j = 0; j < nds.nodes.length; j++) {
+				loadResourceXmlAndParseToJs("http://api.openstreetmap.org/api/0.6/node/" + nds.nodes[j].ref, false, function(result, isWriteToFileAsJson) {
+					var node = extractNodeFromOsm(result, false);
+					
+					for(var i = 0; i < nds.nodes.length; i++) {
+						if(nds.nodes[i].ref == node.id) {
+
+							nds.nodes[i].lat = node.lat;
+							nds.nodes[i].lon = node.lon;
+						}
+					}
+				});
+			}
 		});
-	});
+	}
+
+	setTimeout(function() {
+		writeToFileAsJson(finalWays);
+	}, 60000 * 12)
+	
+	
 });
 
 function loadResourceXmlAndParseToJs(url, isWriteToFileAsJson, callback) {
@@ -57,21 +84,28 @@ function extractWaysFromRelation(relation, isWriteToFileAsJson) {
 	 var result = [];
 	 for(var i = 0, len = relation[0].member.length; i < len; i++) {
 	 	result[i] = relation[0].member[i].$;
+	 	result[i].indexInRelation = i;
 	 }
 
 	 return result;
 }
 
 function extractNdsFromWay(way, isWriteToFileAsJson) {
+
 	var nds = way.osm.way[0].nd;
 
 	if(isWriteToFileAsJson) {
-		writeToFileAsJson(nds);
+		writeToFileAsJson(way);
 	}
 
-	var result = [];
+	var result = {
+		id: way.osm.way[0].$.id,
+		nodes: []
+	};
+
 	for(var i = 0; i < nds.length; i++) {
-		result[i] = nds[i].$;
+		result.nodes[i] = nds[i].$;
+		result.nodes[i].wayId = way.osm.way[0].$.id
 	}
 
 	return result;
@@ -86,8 +120,8 @@ function extractNodeFromOsm(obj, isWriteToFileAsJson) {
 	return obj.osm.node[0].$; 
 }
 
-function writeToFileAsJson(xml) {
-	var jsonString = JSON.stringify( xml, null, 4 );
+function writeToFileAsJson(obj) {
+	var jsonString = JSON.stringify( obj, null, 4 );
 
     fs.writeFile("test.json", jsonString, function(err) {
 	    if(err) {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,8 +3,14 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope,$http) {
 
 	(function() {
-		$http.get('/location_7').success(function(data) {
-			$scope.addLocations(data)
+		$http.get('/eurovelo_6').success(function(data) {
+			console.log(data)
+
+			for(var i = 0; i < data.length; i++) {
+				$scope.addLocations(data[i].nodes)
+			}
+			
+			
 		});
 	})()
 
@@ -22,40 +28,59 @@ angular.module('starter.controllers', [])
 		return "haho"
 	}
 
+	$scope.map = undefined;
+
 	$scope.addLocations = function(locations) {
 
+		if($scope.map == undefined) {
+			var mapProp = {
+				center:new google.maps.LatLng(locations[0].lat,locations[0].lon),//new google.maps.LatLng( location.coords.latitude, location.coords.longitude ),
+				zoom: 12,
+				mapTypeId:google.maps.MapTypeId.ROADMAP
+			};
 
-		var mapProp = {
-			center:new google.maps.LatLng(locations[4].path[0].lat, -1.6676415),//new google.maps.LatLng( location.coords.latitude, location.coords.longitude ),
-			zoom: 12,
-			mapTypeId:google.maps.MapTypeId.ROADMAP
-		};
-
-		$scope.map = new google.maps.Map( document.getElementById( "googleMap" ), mapProp );
+			$scope.map = new google.maps.Map( document.getElementById( "googleMap" ), mapProp );
+		}
 
 		var flightPlanCoordinates = [];
 
 		for(var i = 0; i < locations.length; i++) {
-
-			var path = locations[i].path;
-			for(var j = 0; j < path.length; j++) {
-				console.log(path[j].lat + ", " + path[j].lon)
-				flightPlanCoordinates.push(
-		  			new google.maps.LatLng(path[j].lat, path[j].lon)
-		  		)
-			}
-
-			var flightPath = new google.maps.Polyline({
-			    path: flightPlanCoordinates,
-			    geodesic: true,
-			    strokeColor: '#FF0000',
-			    strokeOpacity: 1.0,
-			    strokeWeight: 2
-			});
-
-			flightPath.setMap($scope.map);
-			flightPlanCoordinates = []
+		  	flightPlanCoordinates.push(
+		  		new google.maps.LatLng(locations[i].lat, locations[i].lon)
+		  	)
 		}
+
+		var flightPath = new google.maps.Polyline({
+		    path: flightPlanCoordinates,
+		    geodesic: true,
+		    strokeColor: '#FF0000',
+		    strokeOpacity: 1.0,
+		    strokeWeight: 2
+		});
+
+		flightPath.setMap($scope.map);
+
+		// for(var i = 0; i < locations.length; i++) {
+
+		// 	var path = locations[i].path;
+		// 	for(var j = 0; j < path.length; j++) {
+		// 		console.log(path[j].lat + ", " + path[j].lon)
+		// 		flightPlanCoordinates.push(
+		//   			new google.maps.LatLng(path[j].lat, path[j].lon)
+		//   		)
+		// 	}
+
+		// 	var flightPath = new google.maps.Polyline({
+		// 	    path: flightPlanCoordinates,
+		// 	    geodesic: true,
+		// 	    strokeColor: '#FF0000',
+		// 	    strokeOpacity: 1.0,
+		// 	    strokeWeight: 2
+		// 	});
+
+		// 	flightPath.setMap($scope.map);
+		// 	flightPlanCoordinates = []
+		// }
 
 		// for(var i = 0; i < locations.length; i++) {
 		  	// flightPlanCoordinates.push(
