03a25aedaf3df913d2e3622faf78ce3c60f34845
gsanta <santa_gergely@hotmail.com>
2014-08-02 20:30:39 +0200

    Revert "Test commit from Eclipse EGit."
    This reverts commit 05d4f74f18717bc64908dd11fef2de30c9e4f1f8.

--- www/js/services/index.ts
+++ www/js/services/index.ts
@@ -6,7 +6,6 @@ import DataProviderService = require('services/DataProviderService');
 
 var services = angular.module('services', []);
 
-
 services.factory('MapService', MapService);
 services.factory('MarkerService', MarkerService);
 services.factory('DataProviderService', DataProviderService);
